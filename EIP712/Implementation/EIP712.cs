c394731750a0c21dcc2950243c9023c8f4474418
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-13 23:30:11 +0200

    Add EIP712 class

---
+++ EIP712/Implementation/EIP712.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EIP712
+{
+    public static class EIP712
+    {
+        public static byte[] Encode<T>(T structure, EIP712Domain domain) where T : class
+        {
+            byte[] domainSeparator = HashStruct(domain);
+            byte[] hash = HashStruct(structure);
+            byte[] result = ByteUtil.Merge(Header, domainSeparator, hash);
+            return result;
+        }
+    }
+}
