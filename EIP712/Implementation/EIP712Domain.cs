00eb67f75d1fd843c01e99cd3dfdb182867db921
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-13 23:20:00 +0200

    Add EIP712Domain class

---
+++ EIP712/Implementation/EIP712Domain.cs
@@ -0,0 +1,24 @@
+﻿using EIP712.Attributes;
+using System.Numerics;
+
+namespace EIP712
+{
+    public class EIP712Domain
+    {
+        // TODO: Add docs
+        [StructType("string", 1)]
+        public string Name { get; set; }
+
+        [StructType("string", 2)]
+        public string Version { get; set; }
+
+        [StructType("uint256", 3)]
+        public BigInteger? ChainId { get; set; }
+
+        [StructType("address", 4)]
+        public string VerifyingContract { get; set; }
+
+        [StructType("bytes32", 5)]
+        public string Salt { get; set; }
+    }
+}
