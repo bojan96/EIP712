3df1bfc6b0025c68398d953af576216764c082b9
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-14 23:58:13 +0200

    Add TestType typed structure used for testing

---
+++ Tests/TestType.cs
@@ -0,0 +1,16 @@
+﻿using EIP712.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class TestType
+    {
+        [StructType("string", 1)]
+        public string StringType { get; set; }
+
+        [StructType("address", 2)]
+        public string AddressType { get; set; }
+    }
+}
