e34f7b6324783bd079d143edf98bff257cc87f14
Bojan Malinic <bojan.malinic@outlook.com>
2019-04-15 11:06:24 +0200

    Add EthereumSignature class

---
+++ EIP712/Implementation/EthereumSignature.cs
@@ -0,0 +1,37 @@
+﻿using Nethereum.Util;
+
+namespace EIP712.Implementation
+{
+    public class EthereumSignature
+    {
+        internal EthereumSignature(byte[] r, byte[] s, byte[] v)
+        {
+            R = r;
+            S = s;
+            V = v;
+        }
+
+        /// <summary>
+        /// R
+        /// </summary>
+        public byte[] R { get; }
+
+        /// <summary>
+        /// S
+        /// </summary>
+        public byte[] S { get; }
+
+        /// <summary>
+        /// Recovery value
+        /// </summary>
+        public byte[] V { get; }
+
+        /// <summary>
+        /// Ethereum Signature in packed format (i.e. R, S, V) concatenated together
+        /// </summary>
+        /// <returns> Instance of <see cref="EthereumSignature"/></returns>
+        public byte[] GetPacked()
+            => ByteUtil.Merge(R, S, V);
+
+    }
+}
