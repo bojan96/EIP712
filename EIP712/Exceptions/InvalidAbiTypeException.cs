7df1f4f9d2d258cc40710de9063b72a95e991067
Bojan Malinic <bojan.malinic@outlook.com>
2019-05-18 23:15:16 +0200

    Add exception for invalid abi types

---
+++ EIP712/Exceptions/InvalidAbiTypeException.cs
@@ -0,0 +1,15 @@
+﻿namespace EIP712.Exceptions
+{
+    public class InvalidAbiTypeException : Eip712Exception
+    {
+        internal InvalidAbiTypeException(string propertyName, string abiType) 
+            : base($"Invalid ABI type \"${abiType}\" for property \"${propertyName}\"", null)
+        {
+            PropertyName = propertyName;
+            AbiType = abiType;
+        }
+
+        public string PropertyName { get; }
+        public string AbiType { get; }
+    }
+}
